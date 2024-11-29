019ba32206640ae34e0dafe96ffea5221e3151c1
Sanskar-1singh <sanskarsingh812@gmail.com>
2024-11-29 10:01:35 +0530

    successsfully conncted to DB

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const fastify=require('fastify')({logger:true});//immediately called fastify constructor>>
 
 const app=require('./app');
+const connectToDB = require('./config/db.config');
 
 const serverConfig=require('./config/serverConfig');
 
 fastify.register(app);  
-fastify.listen({port:serverConfig.PORT},(err)=>{
+fastify.listen({port:serverConfig.PORT},async(err)=>{
     if(err){
         fastify.log.error(err);
         process.exit(1);
     }
+    await connectToDB();
     console.log(`server up at port ${serverConfig.PORT}`);
 })
